0d82e15a7f84e60255d0d475e334cf956e926d09
Michael Nesterenko <misha.nesterenko@gmail.com>
2012-03-28 00:28:18 +0300

    preparation before repository merge

--- src/main/java/mishanesterenko/iad/lb3/loader/Loader.java
+++
@@ -1,8 +0,0 @@
-package mishanesterenko.iad.lb3.loader;
-
-import mishanesterenko.iad.lb3.util.Dataset;
-
-public interface Loader {
-	public void load(Dataset ds) throws Exception;
-
-}
--- src/main/java/mishanesterenko/iad/lb3/util/StringAttribute.java
+++
@@ -1,9 +0,0 @@
-package mishanesterenko.iad.lb3.util;
-
-import java.util.List;
-
-public class StringAttribute extends Attribute<String> {
-	public StringAttribute(String lbl, List<String> vals, boolean categorial) {
-		super(lbl, vals, categorial);
-	}
-}
